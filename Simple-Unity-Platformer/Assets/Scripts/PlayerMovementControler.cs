2acf5f6d88c3a1b4cd54bb8f9640ffbe133ae8c1
horotv <tim.nelke@outlook.com>
2017-09-29 16:43:35 +0200

    update

---
+++ Simple-Unity-Platformer/Assets/Scripts/PlayerMovementControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementControler : MonoBehaviour {
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    } 
+
+    void FixedUpdate ()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        rb.AddForce(new Vector3(horizontalInput, verticalInput));
+    }
+}
