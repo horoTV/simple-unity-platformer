832bf75c7c5843d28ec769b68b5b23353ea1b521
horotv <tim.nelke@outlook.com>
2017-09-29 19:44:03 +0200

    Updated PlayerMovementControler, need to work on jump behavior

--- Simple-Unity-Platformer/Assets/Scripts/PlayerMovementControler.cs
+++ Simple-Unity-Platformer/Assets/Scripts/PlayerMovementControler.cs
@@ -1,23 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementControler : MonoBehaviour {
-    public float speed;
+    public float speed = 15f;
+    public float jumpForce = 1000f;
+    public bool jump = false;
+    public Transform groundCheck;
+    public bool facingLeft = true;
+    public bool grounded = false;
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb2d;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-    } 
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        grounded = Physics2D.OverlapArea(transform.position, groundCheck.position, LayerMask.NameToLayer("Ground"));
+
+        if(Input.GetAxis("Vertical") > 0 && grounded)
+        {
+            jump = true;
+        }
+    }
 
     void FixedUpdate ()
     {
-        //var verticalInput = Input.GetAxis("Vertical") * speed;
-        var verticalInput = 0f;
         var horizontalInput = Input.GetAxis("Horizontal") * speed;
 
-        rb.AddForce(new Vector3(horizontalInput, verticalInput));
+        if (horizontalInput > 0 && facingLeft)
+            Flip();
+        else if (horizontalInput < 0 & !facingLeft)
+            Flip();
+
+        if (jump)
+        {
+            jump = false;
+            rb2d.AddForce(new Vector2(0, jumpForce));   
+        }
+
+        rb2d.AddForce(new Vector3(horizontalInput, 0));
+    }
+
+    private void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
